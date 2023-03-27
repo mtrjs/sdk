e6091391391d66f25d309a783972ade112fce9d6
xjq <lebnizjoan@gmail.com>
2023-03-27 13:54:35 +0800

    fix: traceId 生成规则改用 uuid

--- packages/core/builder.ts
+++ packages/core/builder.ts
@@ -1,9 +1,16 @@
-// import { generateRandom } from '@tubit/common/lib/random';
 import pkg from '../../package.json';
 import { IBaseData, LData } from './type';
 
 interface Config {
   appId: string;
+}
+
+function guid() {
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
+    const r = (Math.random() * 16) | 0,
+      v = c == 'x' ? r : (r & 0x3) | 0x8;
+    return v.toString(16);
+  });
 }
 
 /**
@@ -18,7 +25,7 @@ export class Builder {
 
   constructor(config: Config) {
     const { appId } = config;
-    const traceId = 'generateRandom()';
+    const traceId = guid();
     this.cache = new Map();
 
     this.baseData = {
